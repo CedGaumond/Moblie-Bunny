916d3cf829011591fa1cfb5f885f574e0ba64f0c
CedGaumond <145615267+CedGaumond@users.noreply.github.com>
2025-02-17 08:18:14 -0800

    I remade the whole project.
    I created a better .gitignore to ensure we only push the necessary data.

---
+++ mobile_bunny/lib/main.dart
@@ -0,0 +1,52 @@
+// lib/main.dart
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+import 'presentation/pages/home_page.dart';
+import 'presentation/pages/login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: ".env");
+    print("✅ .env file loaded successfully!");
+  } catch (e) {
+    print("❌ Error loading .env file: $e");
+  }
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Bunny&Co',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return snapshot.hasData ? const HomePage() : const LoginPage();
+          },
+        ),
+      ),
+    );
+  }
+}
